b2c67d9caefb71336c16724c6b688a0cfb8b8b2d
Owen Butler <179979+owenbutler@users.noreply.github.com>
2024-07-21 22:19:47 +1000

    stub keyboard docs

--- keyboard.rb
+++ keyboard.rb
@@ -1,5 +1,39 @@
 module GTK
   class Keyboard
 
+    # @return [Integer] Returns Kernel.tick_count if any keys on the keyboard were pressed.
+    attr_reader :active
+
+    # @return [true,false] Returns true if the game has keyboard focus.
+    attr_reader :has_focus
+
+    # @return [true,false] Returns true if up or w is pressed or held on the keyboard.
+    attr_reader :up
+
+    # @return [true,false] Returns true if down or s is pressed or held on the keyboard.
+    attr_reader :down
+
+    # @return [true,false] Returns true if left or a is pressed or held on the keyboard.
+    attr_reader :left
+
+    # @return [true,false] Returns true if right or d is pressed or held on the keyboard.
+    attr_reader :right
+
+    # @return [Integer] Returns -1 (left), 0 (neutral), or +1 (right) depending on results of #left and #right.
+    attr_reader :left_right
+
+    # @return [Integer] Returns -1 (left), 0 (neutral), or +1 (right) depending on results of #up and #up.
+    attr_reader :up_down
+
+    attr_reader :key_down
+
+    attr_reader :key_held
+
+    attr_reader :key_up
+
+    # @return [Hash] Returns a Hash with all keys on the keyboard in their respective state.
+    #   The Hash contains the following keys
+    #   :down, :held, :down_or_held, :up
+    attr_reader :keys
   end
 end
