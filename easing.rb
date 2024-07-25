0b759d1b467fdaed31477c56a6ea1d8a85aac136
Owen Butler <179979+owenbutler@users.noreply.github.com>
2024-07-25 15:18:08 +1000

    pass at easing doco

--- easing.rb
+++ easing.rb
@@ -1,9 +1,23 @@
 module GTK
+  # A set of functions that allow you to determine the current progression of an easing function.
   class Easing
 
+    # Given a start, current, duration, and a multiple bezier values, this function returns a number between 0 and 1 that represents the progress of an easing function.
+    #
+    # @param start_tick [Integer]
+    # @param current_tick [Integer]
+    # @param duration [Integer] duration in ticks
+    # @param spline [Array] multiple bezier values
+    # @return [Float] returns a number between 0 and 1 that represents the progress of an easing function.
     def ease_spline start_tick, current_tick, duration, spline; end
 
-    # todo is it *definitions?
+    # Given a start, current, duration, and easing definitions.  This function returns a number between 0 and 1 that represents the progress of an easing function.
+    #
+    # @param start_tick [Integer]
+    # @param current_tick [Integer]
+    # @param duration [Integer] duration in ticks
+    # @param definitions [Object] easing definitions
+    # @return [Float] returns a number between 0 and 1 that represents the progress of an easing function.
     def ease start_tick, current_tick, duration, definitions; end
   end
 end
