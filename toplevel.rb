d7ba89e2d48041001d59abdc3e99848e5b3ad7d9
leviongit <levi.duncan.self@gmail.com>
2024-08-01 20:44:07 +0200

    qol: add abstract tick func + dr callbacks

---
+++ toplevel.rb
@@ -0,0 +1,13 @@
+# @!method tick(args)
+#   @abstract tick function, override it with your game
+#   @param args [GTK::Args]
+#   @return [void]
+
+# @!method boot(args)
+#   @abstract boot function, override for setup that has to happen once, at the very beginning of the dr process
+#   @param args [GTK::Args]
+#   @return [void]
+
+# @!method reset(args)
+#   @param args [GTK::Args]
+#   @return [void]
